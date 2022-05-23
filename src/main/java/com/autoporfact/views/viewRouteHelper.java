73dd816ea776001fae9ecb51214ee26496037d00
frank <francisco.graya26@gmail.com>
2022-05-23 16:09:05 -0500

    se agrego configuracion principal

---
+++ src/main/java/com/autoporfact/views/viewRouteHelper.java
@@ -0,0 +1,25 @@
+package com.autoporfact.views;
+
+public class viewRouteHelper {
+
+
+    //home
+    public final static String PLANTILLA = "plantilla";
+
+
+    //login
+    public final static String LOGIN = "login";
+
+    //home
+    public final static String HOME = "home";
+
+    //register
+    public final static String REGISTER = "register";
+
+    //Views
+    public final static String INDEX1 = "home/index";
+    public final static String HELLO = "home/hello";
+
+    //Redirects
+    public final static String ROUTE_INDEX = "/index";
+}
