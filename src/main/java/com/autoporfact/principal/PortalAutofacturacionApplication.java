5671cb8ff0ae86f1fb029a37e7e934053767fc05
frank <francisco.graya26@gmail.com>
2022-05-23 14:02:09 -0500

    primer commit de arranque

---
+++ src/main/java/com/autoporfact/principal/PortalAutofacturacionApplication.java
@@ -0,0 +1,13 @@
+package com.autoporfact.principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalAutofacturacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalAutofacturacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autoporfact/principal/PortalAutofacturacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoporfact.principal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalAutofacturacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
