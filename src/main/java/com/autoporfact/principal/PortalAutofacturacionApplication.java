489b1ec01ee7762ea13d36b232e7246716305976
frank <francisco.graya26@gmail.com>
2022-05-23 18:30:32 -0500

    se resolvio problema de librerias de js

--- src/main/java/com/autoporfact/principal/PortalAutofacturacionApplication.java
+++ src/main/java/com/autoporfact/principal/PortalAutofacturacionApplication.java
@@ -7,20 +7,13 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
-public class PortalAutofacturacionApplication extends WebMvcConfigurationSupport {
+public class PortalAutofacturacionApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortalAutofacturacionApplication.class, args);
 	}
 
 	// El paquete de guía de ruta para configurar archivos de recursos estáticos aquí se importa directamente de forma predeterminada.
-	@Override
-	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
-		registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/").resourceChain(true);
-		registry.addResourceHandler("/js/**").addResourceLocations("/js/").resourceChain(true);
-		registry.addResourceHandler("/css/**").addResourceLocations("/css/").resourceChain(true);
-		super.addResourceHandlers(registry);
-	}
+
 
 }
