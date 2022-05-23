3ab6a4b9c42af6e2968b23d62ce574b62bd7e820
frank <francisco.graya26@gmail.com>
2022-05-23 17:55:24 -0500

    Revisar WebMvcConfigurationSupport

--- src/main/java/com/autoporfact/principal/PortalAutofacturacionApplication.java
+++ src/main/java/com/autoporfact/principal/PortalAutofacturacionApplication.java
@@ -2,12 +2,26 @@ package com.autoporfact.principal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
-public class PortalAutofacturacionApplication {
+public class PortalAutofacturacionApplication extends WebMvcConfigurationSupport {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortalAutofacturacionApplication.class, args);
+	}
+
+	// El paquete de guía de ruta para configurar archivos de recursos estáticos aquí se importa directamente de forma predeterminada.
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/").resourceChain(false);
+		registry.addResourceHandler("/js/**").addResourceLocations("/js/").resourceChain(false);
+		registry.addResourceHandler("/css/**").addResourceLocations("/css/").resourceChain(false);
+		super.addResourceHandlers(registry);
+
 	}
 
 }
